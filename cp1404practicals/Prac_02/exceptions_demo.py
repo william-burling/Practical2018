68d9058765056c9c39e69b3efca405a64ad04500
william-burling <william.burling@my.jcu.edu.au>
2018-08-30 15:35:36 +1000

    Add files via upload

---
+++ cp1404practicals/Prac_02/exceptions_demo.py
@@ -0,0 +1,14 @@
+try:
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    fraction = numerator / denominator
+    print(fraction)
+except ValueError:
+    print("Numerator and denominator must be valid numbers!")
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
+
+
+#Value Error occurs when no numbers are entered
+#ZeroDivisionError occurs when the numbers entered can't be divided
+
